30be98fc15c1fda631c7b25870939d37997202a2
Salva Moreno <morenosanchezsalva@gmail.com>
2023-12-17 14:20:32 +0100

    Create first project structure

---
+++ DragonBallAppFundamentos/app/src/main/java/com/example/dragonballappfundamentos/ui/home/HomeActivity.kt
@@ -0,0 +1,12 @@
+package com.example.dragonballappfundamentos.ui.home
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.dragonballappfundamentos.R
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
