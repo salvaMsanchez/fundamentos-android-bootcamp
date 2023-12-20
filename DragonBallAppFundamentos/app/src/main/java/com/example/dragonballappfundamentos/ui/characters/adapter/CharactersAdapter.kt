cfe8c73e2eb806892857a4beac62e541a8e42459
Salva Moreno <morenosanchezsalva@gmail.com>
2023-12-20 22:05:38 +0100

    Sketch CharactersAdapter

--- DragonBallAppFundamentos/app/src/main/java/com/example/dragonballappfundamentos/ui/characters/adapter/CharactersAdapter.kt
+++ DragonBallAppFundamentos/app/src/main/java/com/example/dragonballappfundamentos/ui/characters/adapter/CharactersAdapter.kt
@@ -1,4 +1,25 @@
 package com.example.dragonballappfundamentos.ui.characters.adapter
 
-class CharactersAdapter {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.dragonballappfundamentos.R
+import com.example.dragonballappfundamentos.ui.characters.model.Character
+
+class CharactersAdapter(
+    var characters: List<Character> = emptyList(),
+    private val onItemSelected: (String) -> Unit
+):
+    RecyclerView.Adapter<CharactersViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CharactersViewHolder {
+        return CharactersViewHolder(
+            LayoutInflater.from(parent.context).inflate(R.layout.item_character, parent, false)
+        )
+    }
+
+    override fun onBindViewHolder(viewHolder: CharactersViewHolder, position: Int) {
+        viewHolder.bind(characters[position], onItemSelected)
+    }
+
+    override fun getItemCount(): Int = characters.size
 }
--- DragonBallAppFundamentos/app/src/main/java/com/example/dragonballappfundamentos/ui/characters/adapter/CharactersViewHolder.kt
+++ DragonBallAppFundamentos/app/src/main/java/com/example/dragonballappfundamentos/ui/characters/adapter/CharactersViewHolder.kt
@@ -1,4 +1,15 @@
 package com.example.dragonballappfundamentos.ui.characters.adapter
 
-class CharactersViewHolder {
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+import com.example.dragonballappfundamentos.databinding.ItemCharacterBinding
+import com.example.dragonballappfundamentos.ui.characters.model.Character
+
+class CharactersViewHolder(view: View): RecyclerView.ViewHolder(view) {
+
+    private var binding = ItemCharacterBinding.bind(view)
+
+    fun bind(character: Character, onItemSelected: (String) -> Unit) {
+
+    }
 }
